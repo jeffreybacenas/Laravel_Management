7318c2ad63d7f6ae8c79576fef4b2b4dded9497b
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-08-27 22:05:43 +0800

    Finishing Dashboard Module

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,112 +1,12 @@
 (function($) {
   'use strict';
   $(function() {
-    if ($("#performaneLine").length) {
-      var graphGradient = document.getElementById("performaneLine").getContext('2d');
-      var graphGradient2 = document.getElementById("performaneLine").getContext('2d');
-      var saleGradientBg = graphGradient.createLinearGradient(5, 0, 5, 100);
-      saleGradientBg.addColorStop(0, 'rgba(26, 115, 232, 0.18)');
-      saleGradientBg.addColorStop(1, 'rgba(26, 115, 232, 0.02)');
-      var saleGradientBg2 = graphGradient2.createLinearGradient(100, 0, 50, 150);
-      saleGradientBg2.addColorStop(0, 'rgba(0, 208, 255, 0.19)');
-      saleGradientBg2.addColorStop(1, 'rgba(0, 208, 255, 0.03)');
-      var salesTopData = {
-          labels: ["SUN","sun", "MON", "mon", "TUE","tue", "WED", "wed", "THU", "thu", "FRI", "fri", "SAT"],
-          datasets: [{
-              label: 'This week',
-              data: [50, 110, 60, 290, 200, 115, 130, 170, 90, 210, 240, 280, 200],
-              backgroundColor: saleGradientBg,
-              borderColor: [
-                  '#1F3BB3',
-              ],
-              borderWidth: 1.5,
-              fill: true, // 3: no fill
-              pointBorderWidth: 1,
-              pointRadius: [4, 4, 4, 4, 4,4, 4, 4, 4, 4,4, 4, 4],
-              pointHoverRadius: [2, 2, 2, 2, 2,2, 2, 2, 2, 2,2, 2, 2],
-              pointBackgroundColor: ['#1F3BB3)', '#1F3BB3', '#1F3BB3', '#1F3BB3','#1F3BB3)', '#1F3BB3', '#1F3BB3', '#1F3BB3','#1F3BB3)', '#1F3BB3', '#1F3BB3', '#1F3BB3','#1F3BB3)'],
-              pointBorderColor: ['#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff',],
-          },{
-            label: 'Last week',
-            data: [30, 150, 190, 250, 120, 150, 130, 20, 30, 15, 40, 95, 180],
-            backgroundColor: saleGradientBg2,
-            borderColor: [
-                '#52CDFF',
-            ],
-            borderWidth: 1.5,
-            fill: true, // 3: no fill
-            pointBorderWidth: 1,
-            pointRadius: [0, 0, 0, 4, 0],
-            pointHoverRadius: [0, 0, 0, 2, 0],
-            pointBackgroundColor: ['#52CDFF)', '#52CDFF', '#52CDFF', '#52CDFF','#52CDFF)', '#52CDFF', '#52CDFF', '#52CDFF','#52CDFF)', '#52CDFF', '#52CDFF', '#52CDFF','#52CDFF)'],
-              pointBorderColor: ['#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff',],
-        }]
-      };
-  
-      var salesTopOptions = {
-        responsive: true,
-        maintainAspectRatio: false,
-          scales: {
-              yAxes: [{
-                  gridLines: {
-                      display: true,
-                      drawBorder: false,
-                      color:"#F0F0F0",
-                      zeroLineColor: '#F0F0F0',
-                  },
-                  ticks: {
-                    beginAtZero: false,
-                    autoSkip: true,
-                    maxTicksLimit: 4,
-                    fontSize: 10,
-                    color:"#6B778C"
-                  }
-              }],
-              xAxes: [{
-                gridLines: {
-                    display: false,
-                    drawBorder: false,
-                },
-                ticks: {
-                  beginAtZero: false,
-                  autoSkip: true,
-                  maxTicksLimit: 7,
-                  fontSize: 10,
-                  color:"#6B778C"
-                }
-            }],
-          },
-          legend:false,
-          legendCallback: function (chart) {
-            var text = [];
-            text.push('<div class="chartjs-legend"><ul>');
-            for (var i = 0; i < chart.data.datasets.length; i++) {
-              console.log(chart.data.datasets[i]); // see what's inside the obj.
-              text.push('<li>');
-              text.push('<span style="background-color:' + chart.data.datasets[i].borderColor + '">' + '</span>');
-              text.push(chart.data.datasets[i].label);
-              text.push('</li>');
-            }
-            text.push('</ul></div>');
-            return text.join("");
-          },
-          
-          elements: {
-              line: {
-                  tension: 0.4,
-              }
-          },
-          tooltips: {
-              backgroundColor: 'rgba(31, 59, 179, 1)',
-          }
-      }
-      var salesTop = new Chart(graphGradient, {
-          type: 'line',
-          data: salesTopData,
-          options: salesTopOptions
-      });
-      document.getElementById('performance-line-legend').innerHTML = salesTop.generateLegend();
-    }
+    var bookCount = document.getElementById('bookCount').value;
+    var magazineCount = document.getElementById('magazineCount').value;
+    var dvdCount = document.getElementById('dvdCount').value;
+    var userCount = document.getElementById('userCount').value;
+
+    
     if ($("#performaneLine-dark").length) {
       var graphGradient = document.getElementById("performaneLine-dark").getContext('2d');
       var graphGradient2 = document.getElementById("performaneLine-dark").getContext('2d');
@@ -213,13 +113,7 @@
       });
       document.getElementById('performance-line-legend-dark').innerHTML = salesTopDark.generateLegend();
     }
-    if ($("#datepicker-popup").length) {
-      $('#datepicker-popup').datepicker({
-        enableOnReadonly: true,
-        todayHighlight: true,
-      });
-      $("#datepicker-popup").datepicker("setDate", "0");
-    }
+    
     if ($("#status-summary").length) {
       var statusSummaryChartCanvas = document.getElementById("status-summary").getContext('2d');;
       var statusData = {
@@ -368,11 +262,13 @@
     }
     if ($("#marketingOverview").length) {
       var marketingOverviewChart = document.getElementById("marketingOverview").getContext('2d');
+      
+
       var marketingOverviewData = {
-          labels: ["JAN","FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+          labels: ["Total Books","Total Magazines", "Total DVDs", "Total User"],
           datasets: [{
-              label: 'Last week',
-              data: [110, 220, 200, 190, 220, 110, 210, 110, 205, 202, 201, 150],
+              label: 'Library Collection.',
+              data: [bookCount, magazineCount, dvdCount, userCount],
               backgroundColor: "#52CDFF",
               borderColor: [
                   '#52CDFF',
@@ -380,16 +276,7 @@
               borderWidth: 0,
               fill: true, // 3: no fill
               
-          },{
-            label: 'This week',
-            data: [215, 290, 210, 250, 290, 230, 290, 210, 280, 220, 190, 300],
-            backgroundColor: "#1F3BB3",
-            borderColor: [
-                '#1F3BB3',
-            ],
-            borderWidth: 0,
-            fill: true, // 3: no fill
-        }]
+          }]
       };
   
       var marketingOverviewOptions = {
@@ -554,7 +441,7 @@
       var doughnutChartCanvas = $("#doughnutChart").get(0).getContext("2d");
       var doughnutPieData = {
         datasets: [{
-          data: [40, 20, 30, 10],
+          data: [bookCount, magazineCount, dvdCount, userCount],
           backgroundColor: [
             "#1F3BB3",
             "#FDD0C7",
@@ -571,10 +458,10 @@
   
         // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [
-          'Total',
-          'Net',
-          'Gross',
-          'AVG',
+          'Books',
+          'Magazines',
+          'DVDs',
+          'Users',
         ]
       };
       var doughnutPieOptions = {
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -21,23 +21,27 @@
                         <div class="statistics-details d-flex align-items-center justify-content-between">
                           
                          <div>
-                            <p class="statistics-title">Total Books</p>
-                            <h3 class="rate-percentage">{{ $bookCount }}</h3>
+                              <p class="statistics-title">Total Books</p>
+                              <h3 class="rate-percentage">{{ $bookCount }}</h3>
+                              <input type="text" value="{{ $bookCount }}"  id="bookCount" hidden>
                           </div>
 
                           <div>
                             <p class="statistics-title">Total Magazines</p>
                             <h3 class="rate-percentage">{{ $magazineCount }}</h3>
+                            <input type="text" value="{{ $magazineCount }}"  id="magazineCount" hidden>
                           </div>
 
                           <div>
                             <p class="statistics-title">Total DVDs</p>
                             <h3 class="rate-percentage">{{ $dvdCount }}</h3>
+                            <input type="text" value="{{ $dvdCount }}"  id="dvdCount" hidden>
                           </div>
 
                           <div class="d-none d-md-block">
                             <p class="statistics-title">Total User </p>
                             <h3 class="rate-percentage">{{ $userCount }}</h3>
+                            <input type="text" value="{{ $userCount }}"  id="userCount" hidden>
                           </div>
 
 
@@ -52,11 +56,11 @@
                               <div class="card-body">
                                 <div class="d-sm-flex justify-content-between align-items-start">
                                   <div>
-                                    <h4 class="card-title card-title-dash">Books Overview</h4>
+                                    <h4 class="card-title card-title-dash">Library Resources</h4>
                                   </div>
                                 </div>
                                 <div class="d-sm-flex align-items-center mt-1 justify-content-between">
-                                  <div class="d-sm-flex align-items-center mt-4 justify-content-between"><h2 class="me-2 fw-bold">$36,2531.00</h2><h4 class="me-2">USD</h4><h4 class="text-success">(+1.37%)</h4></div>
+                                  <div class="d-sm-flex align-items-center mt-4 justify-content-between"><h2 class="me-2 fw-bold">Library Collection.</h2></div>
                                   <div class="me-3"><div id="marketing-overview-legend"></div></div>
                                 </div>
                                 <div class="chartjs-bar-wrapper mt-3">
@@ -76,7 +80,7 @@
                                 <div class="row">
                                   <div class="col-lg-12">
                                     <div class="d-flex justify-content-between align-items-center mb-3">
-                                      <h4 class="card-title card-title-dash">Books Category</h4>
+                                      <h4 class="card-title card-title-dash">Library Resources</h4>
                                     </div>
                                     <canvas class="my-auto" id="doughnutChart" height="200"></canvas>
                                     <div id="doughnut-chart-legend" class="mt-5 text-center"></div>
