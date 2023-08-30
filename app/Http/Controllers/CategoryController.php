13320c160b1d355b038055c112e5a52bc6861c5f
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-08-30 20:17:06 +0800

    Category retrieve data from database

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return view('category.index');
+        $categories = Category::All();
+        return view('category.index', compact('categories'));
     }
 }
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -18,148 +18,37 @@
                     <table class="table table-striped">
                       <thead>
                         <tr>
-                          <th>Category ID</th>
-                          <th>Category Name </th>
-                          <th>Category Description </th>
-                          <th>Date Created </th>
+                          <th> Category ID</th>
+                          <th> Category Name </th>
+                          <th> Category Description </th>
+                          <th> Date Created </th>
                           <th> Date Updated </th>
+                          <th> Action </th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td class="py-1">
-                            <img src="../../images/faces/face1.jpg" alt="image"/>
-                          </td>
-                          <td>
-                            Herman Beck
-                          </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            $ 77.99
-                          </td>
-                          <td>
-                            May 15, 2015
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="../../images/faces/face2.jpg" alt="image"/>
-                          </td>
-                          <td>
-                            Messsy Adam
-                          </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            $245.30
-                          </td>
-                          <td>
-                            July 1, 2015
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="../../images/faces/face3.jpg" alt="image"/>
-                          </td>
-                          <td>
-                            John Richards
-                          </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            $138.00
-                          </td>
-                          <td>
-                            Apr 12, 2015
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="../../images/faces/face4.jpg" alt="image"/>
-                          </td>
-                          <td>
-                            Peter Meggik
-                          </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            $ 77.99
-                          </td>
-                          <td>
-                            May 15, 2015
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="../../images/faces/face5.jpg" alt="image"/>
-                          </td>
-                          <td>
-                            Edward
-                          </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            $ 160.25
-                          </td>
-                          <td>
-                            May 03, 2015
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="../../images/faces/face6.jpg" alt="image"/>
-                          </td>
-                          <td>
-                            John Doe
-                          </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            $ 123.21
-                          </td>
-                          <td>
-                            April 05, 2015
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="../../images/faces/face7.jpg" alt="image"/>
-                          </td>
-                          <td>
-                            Henry Tom
-                          </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                          </td>
-                          <td>
-                            $ 150.00
-                          </td>
-                          <td>
-                            June 16, 2015
-                          </td>
-                        </tr>
-                      </tbody>
+                            @forelse($categories as $category)
+                            <tr data-id="{{ $category->id }}">
+                                <td>{{ $category->id }}</td>
+                                <td>{{ $category->name }}</td>
+                                <td style="color: {{ $category->description ? 'inherit' : 'blue' }};">{{ $category->description ? $category->description : 'N/A' }}</td>
+                                <td style="color: {{ $category->created_at ? 'inherit' : 'blue' }};">{{ $category->created_at ? \Carbon\Carbon::parse($category->created_at)->format('M d, Y') : 'N/A' }}</td>
+                                <td style="color: {{ $category->updated_at ? 'inherit' : 'blue' }};">{{ $category->updated_at ? \Carbon\Carbon::parse($category->updated_at)->format('M d, Y') : 'N/A' }}</td>
+                                <td>
+                                    <a class="btn btn-sm btn-primary scrollButton editButton">
+                                        <i class="mdi mdi-pencil"></i> Edit
+                                    </a>
+                                    <button class="btn btn-sm btn-danger deleteButton" data-id="{{ $category->id }}">
+                                      <i class="mdi mdi-delete"></i> Delete
+                                    </button>
+                                </td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="6" class="text-center">No records found.</td>
+                            </tr>
+                            @endforelse
+                        </tbody>
                     </table>
                   </div>
                 </div>
