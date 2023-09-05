d9cee83b851ccdfa2b5c6a06270c171bff951026
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-05 20:50:23 +0800

    Trying somthing out

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -4,8 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-	<link rel="stylesheet" href="assets/css/style.css">
-	<link rel="stylesheet" href='assets/css/boxicons.min.css'>
+	<link rel="stylesheet" href="css/custom-style/style.css">
+	<link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
 </head>
 <body>
 	<div class="wrapper">
