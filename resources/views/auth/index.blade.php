324fbe7cd9829b2b9f3a2bd6be3421003366da01
jeffrey072120 <jeffreybaggayacenas@gmail.com>
2023-09-05 20:46:21 +0800

    adding the new login design or layout

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,92 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="icon" type="image/x-icon" href="{{ asset('icon-library.png') }}">
-  <title> Laravel Management </title>
-
-  <style>
-    body{
-      background-image: url('bg-library.jpg');
-      background-repeat: no-repeat;
-      background-size: cover;
-    }
-  </style> 
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="{{ asset('css/vertical-layout-light/style.css') }}">
-  
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href='assets/css/boxicons.min.css'>
 </head>
 <body>
-  <div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-      <div class="content-wrapper d-flex align-items-center auth px-0">
-        <div class="row w-100 mx-0">
-          <div class="col-lg-4 mx-auto">
-            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-              <div class="brand-logo">
-              <div class="text-center mt-5 fw-light ">
-                  <h3 class="text-primary">Library Management</h3> 
-                </div>
-              </div>
-              <form action="{{ route('performLogin')}}" method="POST" class="pt-3" >
-                @csrf
-                <div class="form-group">
-                  <input type="email" class="form-control form-control-lg" name="email" placeholder="Username">
-                </div>
-                <div class="form-group">
-                  <input type="password" class="form-control form-control-lg" name="password" placeholder="Password">
-                </div>
-                <div class="mt-3 text-center">
-                <button type="submit" class="btn btn-block btn-facebook auth-form-btn center">
-                  <i class="fa fa-facebook"></i> SIGN IN
-                </button>
+	<div class="wrapper">
+		<form action="">
+			<h1>Login</h1>
+			<div class="input-box">
+				<input type="text" placeholder="Username" required>
+				<i class='bx bxs-user'></i>
+			</div>
+			<div class="input-box">
+				<input type="password" placeholder="Password" required>
+				<i class='bx bxs-lock-alt' ></i>
+			</div>
 
-                </div>
-                <div class="my-2 d-flex justify-content-between align-items-center">
-                  <div class="form-check">
-                  </div>
-                </div>
-                <div class="text-center mt-4 fw-light">
-                  Don't have an account? <a href="{{ route('registration') }}" class="text-primary">Create</a>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- content-wrapper ends -->
-    </div>
-    <!-- page-body-wrapper ends -->
-  </div>
-@include('partials._footer')
-@include('partials._script')
 
-@if(session('success'))
-    <script>
-        Swal.fire({
-            icon: 'success',
-            title: '{{ session('success') }}',
-            toast: true,
-            position: 'top-end', // Position the toast notification at the top-right corner
-            showConfirmButton: false,
-            timer: 3000 // Display for 3 seconds
-        });
-    </script>
-@endif
+			<button type="submit" class="btn">Login</button>
 
-@if(Session::has('error'))
-    <script>
-        Swal.fire({
-            icon: 'error',
-            title: '{{ Session::get('error') }}',
-            toast: true,
-            position: 'top-end', // Position the toast notification at the top-right corner
-            showConfirmButton: false,
-            timer: 3000 // Display for 3 seconds
-        });
-    </script>
-@endif
+			<div class="register-link">
+				<p>Don't have an account?
+					<a href="#">Register</a></p>
+			</div>
+		</form>
+	</div>
+	
+</body>
+</html>
